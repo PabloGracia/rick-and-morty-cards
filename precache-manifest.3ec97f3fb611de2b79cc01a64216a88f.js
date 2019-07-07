5712a2b3c344a7202502202b54c3fb298670ac83
Gracia <Pablo.Gracia@ricardo.com>
2019-07-07 15:30:00 +0200

    Updates

---
+++ precache-manifest.3ec97f3fb611de2b79cc01a64216a88f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be72f5fec59985aabe2bb3a63613f603",
+    "url": "/rick-and-morty-cards/index.html"
+  },
+  {
+    "revision": "2a6d64eebda7090b491a",
+    "url": "/rick-and-morty-cards/static/css/main.46e4a8a8.chunk.css"
+  },
+  {
+    "revision": "6ea22432601858ce9ef5",
+    "url": "/rick-and-morty-cards/static/js/2.27e9ad4f.chunk.js"
+  },
+  {
+    "revision": "2a6d64eebda7090b491a",
+    "url": "/rick-and-morty-cards/static/js/main.b9ae7168.chunk.js"
+  },
+  {
+    "revision": "e4a5118ca1c332636955",
+    "url": "/rick-and-morty-cards/static/js/runtime~main.be9720af.js"
+  }
+]);
